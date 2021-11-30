268dc1bbe54e0c52f05e5a375d960200789d1b83
Christophe <christophe@akairos.fr>
2021-11-30 14:08:45 +0100

    fix label struct

--- label.go
+++ label.go
@@ -3,41 +3,12 @@ package paack
 type Label struct {
 	CodCurrency string `json:"cod_currency"`
 	CodValue    int    `json:"cod_value"`
-	Customer    struct {
-		Address        Address `json:"address"`
-		CustomerType   string  `json:"customer_type"`
-		Email          string  `json:"email"`
-		ExternalId     string  `json:"external_id"`
-		FirstName      string  `json:"first_name"`
-		LastName       string  `json:"last_name"`
-		HasGdprConsent bool    `json:"has_gdpr_consent"`
-		OrderRef       string  `json:"order_ref"`
-		Phone          string  `json:"phone"`
-		Language       string  `json:"language"`
-	} `json:"customer"`
+	Customer    Customer `json:"customer"`
 	DeliveryAddress      Address `json:"delivery_address"`
 	DeliveryInstructions string  `json:"delivery_instructions"`
 	DeliveryType         string  `json:"delivery_type"`
-	ExpectedDeliveryTs   struct {
-		End struct {
-			Date string `json:"date"`
-			Time string `json:"time"`
-		} `json:"end"`
-		Start struct {
-			Date string `json:"date"`
-			Time string `json:"time"`
-		} `json:"start"`
-	} `json:"expected_delivery_ts"`
-	ExpectedPickUpTs struct {
-		End struct {
-			Date string `json:"date"`
-			Time string `json:"time"`
-		} `json:"end"`
-		Start struct {
-			Date string `json:"date"`
-			Time string `json:"time"`
-		} `json:"start"`
-	} `json:"expected_pick_up_ts"`
+	ExpectedDelivery     ScheduleSlot `json:"expected_delivery_ts"`
+	ExpectedPickUp       ScheduleSlot `json:"expected_pick_up_ts"`
 	ExternalId           string   `json:"external_id"`
 	InsuredCurrency      string   `json:"insured_currency"`
 	InsuredValue         float64  `json:"insured_value"`

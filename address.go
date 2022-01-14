03ec095da5578563fb1ae89e195db494198ceea5
loïc couturier <loic@akairos.fr>
2022-01-14 11:37:11 +0100

    Add omitempty on json serialization

--- address.go
+++ address.go
@@ -5,6 +5,6 @@ type Address struct {
 	Country  string `json:"country"`
 	County   string `json:"county,omitempty"`
 	Line1    string `json:"line1"`
-	Line2    string `json:"line2"`
+	Line2    string `json:"line2,omitempty"`
 	PostCode string `json:"post_code"`
 }
